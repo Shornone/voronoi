ccebef22cb2cf3b0861fcb0b5040b2c75add2b64
marcusvolz <marcus.volz@outlook.com>
2018-04-28 14:11:44 +1000

    removed dependency on steiner package

--- voronoi_circle.R
+++ voronoi_circle.R
@@ -5,44 +5,38 @@
 library(deldir)
 library(ggart)
 library(ggthemes)
-library(steiner)
 library(tidyverse)
 library(tweenr)
 
-# Generate data
-# set.seed(106)
-# long<-rnorm(100000, -1, 1)
-# lat<-rnorm(100000, -1, 1)
-# df <- data.frame(lat,long)
-
+# Set parameters
 set.seed(101)
-
-n <- 10000
 s <- 24 / 24
 eps <- 0.05
-
-#data <- data.frame(x = runif(n, 0, 1), y = runif(n, 0, s))
-
 n <- 1
 
+# Function for computing n points around a circle with centre c0 and radius r0
+compute_circle <- function(c0, r0, n = 360) {
+  data.frame(i = 1:n) %>%
+    dplyr::mutate(angle = i * (2 * pi / n), x = c0[1] + r0 * cos(angle), y = c0[2] + r0 * sin(angle)) %>%
+    dplyr::select(x, y)
+}
+
+# Compute circle points
 data <- 1:10 %>%
   map_df(~compute_circle(c(0, 0), ., n = 365)) %>%
   mutate(x = jitter(x, 50), y = jitter(y, 50))
 
-#This creates the voronoi line segments
+# Create voronoi line segments
 voronoi <- deldir(data$x, data$y)
 
-#Now we can make a plot
+# Create plot
 p <- ggplot() +
-  geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2), data = voronoi$delsgs, color = "black", lineend = "round", alpha = 0.5) +
-  #geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2), data = voronoi$dirsgs, color = "black", lineend = "round", alpha = 0.5) + 
+  geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2), data = voronoi$delsgs,
+               color = "black", lineend = "round", alpha = 0.5) +
   geom_point(data = data, aes(x, y), size = 1) +
   coord_equal() +
   theme_tufte() +
-  #facet_wrap(~id) +
-  theme_blankcanvas() #+
-  #theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), units = "in")) +
-  #coord_polar()
+  theme_blankcanvas(margin_cm = 0)
 
-ggsave("voronoi-circle-04.png", p, width = 24, height = 24, units = "in", dpi = 300)
-#ggsave("voronoi.tiff", p, width = 24, height = 35, units = "in", dpi = 720)
+# Save plot
+ggsave("voronoi-circle-05.png", p, width = 24, height = 24, units = "in", dpi = 300)
